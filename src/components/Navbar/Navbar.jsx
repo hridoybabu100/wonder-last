efcb9ab707c496f1bdd9de831595f11dec5854c1
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-05-14 01:14:38 +0600

    navlinks path updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,27 @@
+'use client'
 import Image from "next/image";
 import NavImg from "../../../public/assets/Wanderlast-1.png";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+
+    const path = usePathname();
+
   const links = (
     <>
-      <Link href={"/"}>
+      <Link href={"/"} className={`${path === '/' ? "border border-white p-1" : ""}`}>
         <li>Home</li>
       </Link>
-      <Link href={"/destination"}>
+      <Link href={"/destination"} className={`${path === '/destination' ? "border border-white p-1" : ""}`}>
         {" "}
         <li>Destinations</li>
       </Link>
-      <Link href={"/book"}>
+      <Link href={"/book"} className={`${path === '/book' ? "border border-white p-1" : ""}`}>
         {" "}
         <li>My Bookings</li>
       </Link>
-      <Link href={"/admin"}>
+      <Link href={"/admin"} className={`${path === '/admin' ? "border border-white p-1" : ""}`}>
         <li>Admin</li>
       </Link>
     </>
@@ -25,7 +30,7 @@ const Navbar = () => {
     <div>
       <div className="flex justify-between items-center gap-6 p-3 max-w-[85%] mx-auto">
         <div>
-          <ul className="flex gap-2">{links}</ul>
+          <ul className="flex gap-2 items-center">{links}</ul>
         </div>
         <div>
           <aside>
@@ -37,16 +42,16 @@ const Navbar = () => {
             ></Image>
           </aside>
         </div>
-        <div className="flex gap-2">
-          <Link href={"/profile"}>
+        <div className="flex gap-2 items-center">
+          <Link href={"/profile"} className={`${path === '/profile' ? "border border-white p-1" : ""}`}>
             {" "}
             <button className="btn">Profile</button>
           </Link>
-          <Link href={"/login"}>
+          <Link href={"/login"} className={`${path === 'login' ? "border border-white p-1" : ""}`}>
             {" "}
             <button className="btn">Login</button>
           </Link>
-          <Link href={"/register"}>
+          <Link href={"/register"} className={`${path === '/register' ? "border border-white p-1" : ""}`}>
             {" "}
             <button className="btn">SingUp</button>
           </Link>
